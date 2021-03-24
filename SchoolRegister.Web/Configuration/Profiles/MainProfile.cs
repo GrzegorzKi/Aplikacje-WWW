c4bce203cf80beb1a5add689def5b8b6dcb2fed7
Grzegorz Kita <grzegorz.kita@protonmail.com>
2021-03-24 11:11:31 +0100

    Dodanie mapowań encji na ViewModele i odwrotnie

--- SchoolRegister.Web/Configuration/Profiles/MainProfile.cs
+++ SchoolRegister.Web/Configuration/Profiles/MainProfile.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using AutoMapper;
 using SchoolRegister.Model.DataModels;
 using SchoolRegister.ViewModels.VM;
@@ -6,17 +7,57 @@ using SchoolRegister.ViewModels.VM;
 namespace SchoolRegister.Web.Configuration.Profiles {
   public class MainProfile : Profile {
     public MainProfile() {
+      // SubjectService.cs
       CreateMap<Subject, SubjectVm>()
-        .ForMember(dest => dest.TeacherName,
-          x => x.MapFrom(src => $"{src.Teacher.FirstName} {src.Teacher.LastName}"))
-        .ForMember(dest => dest.Groups,
-          x => x.MapFrom(src => src.SubjectGroups.Select(y => y.Group)));
-
+        .ForMember(vm => vm.TeacherName,
+          expression => expression.MapFrom(subject => $"{subject.Teacher.FirstName} {subject.Teacher.LastName}"))
+        .ForMember(vm => vm.Groups,
+          expression => expression.MapFrom(subject => subject.SubjectGroups.Select(group => group.Group)));
       CreateMap<AddOrUpdateSubjectVm, Subject>();
-      CreateMap<Group, GroupVm>();
-      CreateMap<SubjectVm, AddOrUpdateSubjectVm>();
+      CreateMap<SubjectVm, AddOrUpdateSubjectVm>(); // Czy potrzebne?
 
-      // TODO Dorabiamy resztę mapperów!
+      // GroupService.cs
+      CreateMap<Group, GroupVm>();
+      CreateMap<AddOrUpdateGroupVm, Group>();
+      CreateMap<DeleteGroupVm, Group>();
+
+      // GradeService.cs
+      CreateMap<Grade, GradeVm>()
+        .ForMember(vm => vm.GradeValue,
+          expression => expression.MapFrom(grade => nameof(grade.GradeValue)))
+        .ForMember(vm => vm.StudentName,
+          expression => expression.MapFrom(grade => $"{grade.Student.FirstName} {grade.Student.LastName}"));
+      CreateMap<AddGradeToStudentVm, Grade>()
+        .ForMember(grade => grade.GradeValue,
+          expression => expression.MapFrom(vm => Enum.Parse<GradeScale>(vm.GradeValue)));
+
+      // StudentService.cs
+      CreateMap<Student, StudentVm>()
+        .ForMember(vm => vm.StudentName,
+          expression => expression.MapFrom(student => $"{student.FirstName} {student.LastName}"));
+      CreateMap<Parent, ParentVm>()
+        .ForMember(vm => vm.ParentName,
+          expression => expression.MapFrom(parent => $"{parent.FirstName} {parent.LastName}"));
+      CreateMap<AddOrUpdateStudentVm, Student>();
+      // CreateMap<AddStudentToGroupVm, Student>();
+      // CreateMap<RemoveStudentFromGroupVm, Student>();
+
+      // TeacherService.cs
+      CreateMap<Teacher, TeacherVm>()
+        .ForMember(vm => vm.TeacherName,
+          expression => expression.MapFrom(teacher => $"{teacher.FirstName} {teacher.LastName}"));
+      CreateMap<AddOrUpdateTeacherVm, Teacher>();
+      CreateMap<CreateEmailVm, EmailVm>();
+      CreateMap<Teacher, EmailVm>()
+        .ForMember(vm => vm.SenderName,
+          expression => expression.MapFrom(teacher => $"{teacher.FirstName} {teacher.LastName}"))
+        .ForMember(vm => vm.SenderEmail,
+          expression => expression.MapFrom(teacher => teacher.Email));
+      CreateMap<Parent, EmailVm>()
+        .ForMember(vm => vm.RecipientName,
+          expression => expression.MapFrom(parent => $"{parent.FirstName} {parent.LastName}"))
+        .ForMember(vm => vm.RecipientEmail,
+          expression => expression.MapFrom(parent => parent.Email));
     }
   }
 }
