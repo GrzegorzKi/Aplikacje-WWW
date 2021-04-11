073ad559ceb6350cd999cb57f17b3d7dfcf54ee0
Grzegorz Kita <grzegorz.kita@protonmail.com>
2021-04-11 14:11:45 +0200

    Fixed not mapped properties in Student.cs

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace SchoolRegister.Model.DataModels {
   public class Student : User {
@@ -9,11 +11,19 @@ namespace SchoolRegister.Model.DataModels {
     [ForeignKey("Parent")]
     public int? ParentId { get; set; }
 
-    public double AverageGrade { get; }
-
     public virtual IList<Grade> Grades { get; set; }
-    public IDictionary<string, List<GradeScale>> GradesPerSubject { get; }
-    public IDictionary<string, double> AverageGradePerSubject { get; }
+
+    [NotMapped]
+    public double AverageGrade => Grades == null || Grades.Count ==0 ? 0.0d : Math.Round(Grades.Average(g=>(int)g.GradeValue),1);
+    [NotMapped]
+    public IDictionary<string, double> AverageGradePerSubject => Grades?.GroupBy(g => g.Subject.Name)
+      .Select(g => new {SubjectName = g.Key, AvgGrade = Math.Round(g.Average(avg => (int) avg.GradeValue), 1)})
+      .ToDictionary(avg => avg.SubjectName, avg=> avg.AvgGrade) ?? new Dictionary<string,double>();
+    [NotMapped]
+    public IDictionary<string, List<GradeScale>> GradesPerSubject => Grades?.GroupBy(g => g.Subject.Name)
+      .Select(g => new {SubjectName = g.Key, GradeList = g.Select(x => x.GradeValue).ToList()})
+      .ToDictionary(x => x.SubjectName,x => x.GradeList) ?? new Dictionary<string, List<GradeScale>>();
+
     public virtual Group Group { get; set; }
     public virtual Parent Parent { get; set; }
   }
