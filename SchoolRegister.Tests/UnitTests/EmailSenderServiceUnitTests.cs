7aad5ded8f4ecd9269986092532b66239de1fa2e
Grzegorz Kita <grzegorz.kita@protonmail.com>
2021-04-16 07:10:59 +0200

    EmailService Unit tests - needs valid configuration

--- SchoolRegister.Tests/UnitTests/EmailSenderServiceUnitTests.cs
+++ SchoolRegister.Tests/UnitTests/EmailSenderServiceUnitTests.cs
@@ -1,5 +1,9 @@
-﻿using SchoolRegister.DAL.EF;
+﻿using System;
+using System.Linq;
+using SchoolRegister.DAL.EF;
 using SchoolRegister.Services.Interfaces;
+using SchoolRegister.ViewModels.VM;
+using Xunit;
 
 namespace SchoolRegister.Tests.UnitTests {
   public class EmailSenderServiceUnitTests : BaseUnitTests {
@@ -7,6 +11,73 @@ namespace SchoolRegister.Tests.UnitTests {
 
     public EmailSenderServiceUnitTests(ApplicationDbContext dbContext, IEmailSenderService emailSenderService) : base(dbContext) {
       _emailSenderService = emailSenderService;
+    }
+
+    [Fact]
+    public void CreateEmail() {
+      var createEmailVm = new CreateEmailVm {
+        SenderId = 1,
+        RecipientId = 4,
+        Subject = "Test email message",
+        Body = "This is a test message body."
+      };
+
+      var emailTask = _emailSenderService.SendEmailToParent(createEmailVm);
+      var email = emailTask.Result;
+
+      Assert.NotNull(email);
+    }
+
+    [Fact]
+    public void CreateEmailWithNullSubjectAndBody() {
+      var createEmailVm = new CreateEmailVm {
+        SenderId = 1,
+        RecipientId = 4,
+        Subject = null,
+        Body = null
+      };
+
+      var emailTask = _emailSenderService.SendEmailToParent(createEmailVm);
+      var email = emailTask.Result;
+
+      Assert.NotNull(email);
+    }
+
+    [Fact]
+    public void CreateEmailWithNonTeacherSenderIdShouldThrowException() {
+      var createEmailVm = new CreateEmailVm {
+        SenderId = -1,
+        RecipientId = 1,
+        Subject = "Test email message",
+        Body = "This is a test message body."
+      };
+
+      var emailTask = _emailSenderService.SendEmailToParent(createEmailVm);
+      var exception = Assert.Throws<AggregateException>(() => emailTask.Result);
+      Assert.IsType<InvalidOperationException>(exception.InnerExceptions.First());
+      Assert.Contains("SenderId", exception.Message);
+    }
+
+    [Fact]
+    public void CreateEmailWithNonParentRecipientIdShouldThrowException() {
+      var createEmailVm = new CreateEmailVm {
+        SenderId = 1,
+        RecipientId = -1,
+        Subject = "Test email message",
+        Body = "This is a test message body."
+      };
+
+      var emailTask = _emailSenderService.SendEmailToParent(createEmailVm);
+      var exception = Assert.Throws<AggregateException>(() => emailTask.Result);
+      Assert.IsType<InvalidOperationException>(exception.InnerExceptions.First());
+      Assert.Contains("RecipientId", exception.Message);
+    }
+
+    [Fact]
+    public void CreateEmailWithNullArgumentShouldThrowException() {
+      var gradeTask = _emailSenderService.SendEmailToParent(null);
+      var exception = Assert.Throws<AggregateException>(() => gradeTask.Result);
+      Assert.IsType<ArgumentNullException>(exception.InnerExceptions.First());
     }
   }
 }
