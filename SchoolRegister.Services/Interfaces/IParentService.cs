d93466ed965fbb4c0107313b38be907793bb9b53
Grzegorz Kita <grzegorz.kita@protonmail.com>
2021-04-28 17:47:03 +0200

    Added ParentService

---
+++ SchoolRegister.Services/Interfaces/IParentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using SchoolRegister.Model.DataModels;
+using SchoolRegister.ViewModels.VM;
+
+namespace SchoolRegister.Services.Interfaces {
+  public interface IParentService {
+    ParentVm GetParent(Expression<Func<Parent, bool>> filterExpression);
+    IEnumerable<ParentVm> GetParents(Expression<Func<Parent, bool>> filterExpression = null);
+  }
+}
---
+++ SchoolRegister.Services/Services/ParentService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using SchoolRegister.DAL.EF;
+using SchoolRegister.Model.DataModels;
+using SchoolRegister.Services.Interfaces;
+using SchoolRegister.ViewModels.VM;
+
+namespace SchoolRegister.Services.Services {
+  public class ParentService : BaseService, IParentService {
+    public ParentService(ApplicationDbContext dbContext, IMapper mapper, ILogger logger)
+        : base(dbContext, mapper, logger) { }
+    public ParentVm GetParent(Expression<Func<Parent, bool>> filterExpression) {
+      try {
+        if (filterExpression == null) {
+          throw new ArgumentNullException(nameof(filterExpression), "FilterExpression is null");
+        }
+
+        var parentEntity = DbContext.Parents.FirstOrDefault(filterExpression);
+
+        return Mapper.Map<ParentVm>(parentEntity);
+      }
+      catch (Exception ex) {
+        Logger.LogError(ex, ex.Message);
+        throw;
+      }
+    }
+
+    public IEnumerable<ParentVm> GetParents(Expression<Func<Parent, bool>> filterExpression = null) {
+      try {
+        var parentsEntities = DbContext.Parents.AsQueryable();
+        if (filterExpression != null) parentsEntities = parentsEntities.Where(filterExpression);
+
+        return Mapper.Map<IEnumerable<ParentVm>>(parentsEntities);
+      }
+      catch (Exception ex) {
+        Logger.LogError(ex, ex.Message);
+        throw;
+      }
+    }
+  }
+}
